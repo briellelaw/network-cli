b4a232fbd18e35066651d8c3de7571baf5b03ede
briellelaw <lawbrielle@gmail.com>
2021-10-03 15:19:15 -0700

    Base of network querying cli

--- cmd/my-cli/cli.go
+++ cmd/my-cli/cli.go
@@ -2,8 +2,98 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"net"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("Hello World")	
+	app :=	cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers" 
+
+	flag1 := cli.StringFlag {
+		Name: "host", 
+		Value: "tutorialedge.net",
+	}
+
+	myFlags := []cli.Flag {
+		&flag1,
+	}
+
+	nsCommand := cli.Command {
+		Name: "ns", 
+		Usage: "Looks up the Name Servers for a Particular Host", 
+		Flags: myFlags,
+		Action: func(c *cli.Context) error {
+			ns, err := net.LookupNS(c.String("host"))
+			if err != nil {
+				return err
+			}
+			for i := 0; i < len(ns); i++ {
+				fmt.Println(ns[i].Host)
+			}
+			return nil
+		},
+	}
+
+	ipCommand := cli.Command {
+		Name: "ip", 
+		Usage: "Looks up the IP for a Particular Host", 
+		Flags: myFlags,
+		Action: func(c *cli.Context) error {
+			ip, err := net.LookupIP(c.String("host"))
+			if err != nil {
+				return err
+			}
+			for i := 0; i < len(ip); i++ {
+				fmt.Println(ip[i])
+			}
+			return nil
+		},
+	}
+
+	cnameCommand := cli.Command {
+		Name: "cname", 
+		Usage: "Looks up the CNAME for a Particular Host", 
+		Flags: myFlags,
+		Action: func(c *cli.Context) error {
+			cname, err := net.LookupCNAME(c.String("host"))
+			if err != nil {
+				return err
+			}
+			fmt.Println(cname)
+			return nil
+		},
+	}
+
+	mxCommand := cli.Command {
+		Name: "mx", 
+		Usage: "Looks up the MX records for a Particular Host", 
+		Flags: myFlags,
+		Action: func(c *cli.Context) error {
+			mx, err := net.LookupMX(c.String("host"))
+			if err != nil {
+				return err
+			}
+			for i :=0; i < len(mx); i++ {
+				fmt.Println(mx[i].Host, mx[i].Pref)
+			}
+			return nil
+		},
+	}
+
+
+	app.Commands = []*cli.Command {
+		&nsCommand,
+		&ipCommand,
+		&cnameCommand,
+		&mxCommand,
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
